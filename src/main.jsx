70afc982a17c2aa6fbd34c56c370b620c0fd1a1d
NimeshRukshan <fernandownr.21@uom.lk>
2025-01-02 19:14:40 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import ReactDOM from "react-dom";
+import App from "./App";
+import React from "react";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
---
+++ vite.config.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-undef */
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  base: "/",
+  build: {
+    outDir: "dist", // Ensure this is the default output directory
+  },
+  server: {
+    port: 3000, // Optional: Dev server port
+  },
+});
